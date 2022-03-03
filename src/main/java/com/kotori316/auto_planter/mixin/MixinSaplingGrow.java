d0618c7dfb7a59db5e14b4e557c650bb6a0236ed
Kotori316 <Kotori316@gmail.com>
2022-03-03 19:39:33 +0900

    Use cast to call other methods in mixin

--- src/main/java/com/kotori316/auto_planter/mixin/MixinSaplingGrow.java
+++ src/main/java/com/kotori316/auto_planter/mixin/MixinSaplingGrow.java
@@ -7,7 +7,6 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.SaplingBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -16,7 +15,7 @@ import com.kotori316.auto_planter.AutoPlanter;
 
 @Mixin(SaplingBlock.class)
 public abstract class MixinSaplingGrow {
-    @SuppressWarnings("deprecation")
+    @SuppressWarnings({"deprecation", "ConstantConditions"})
     @Inject(method = "randomTick", at = @At("HEAD"), cancellable = true)
     public void growOnPlanter(BlockState state, ServerLevel worldIn, BlockPos pos, Random random, CallbackInfo ci) {
         if (worldIn.getBlockState(pos.below()).is(AutoPlanter.Holder.PLANTER_UPGRADED_BLOCK)) {
@@ -30,13 +29,10 @@ public abstract class MixinSaplingGrow {
                     // Is this a real sapling? It might be a modified sapling.
                     newState = state;
                 }
-                advanceTree(worldIn, pos, newState, random);
+                ((SaplingBlock) (Object) this).advanceTree(worldIn, pos, newState, random);
                 // AutoPlanter.LOGGER.debug("Tree was grown in #growOnPlanter. {}, {}", state, pos);
             }
             ci.cancel();
         }
     }
-
-    @Shadow
-    public abstract void advanceTree(ServerLevel world, BlockPos pos, BlockState state, Random random);
 }
