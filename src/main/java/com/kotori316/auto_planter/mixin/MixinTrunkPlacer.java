d97f34ad2aed3480a0c040ea79f986b27db792fb
Kotori316 <Kotori316@gmail.com>
2022-04-13 11:04:26 +0900

    Prevent planters from being replaced to dirt. Close #2

--- src/main/java/com/kotori316/auto_planter/mixin/MixinTrunkPlacer.java
+++ src/main/java/com/kotori316/auto_planter/mixin/MixinTrunkPlacer.java
@@ -1,23 +1,28 @@
 package com.kotori316.auto_planter.mixin;
 
+import java.util.Random;
+import java.util.function.BiConsumer;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.LevelSimulatedReader;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.TrunkPlacer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.kotori316.auto_planter.AutoPlanter;
 
 @Mixin(TrunkPlacer.class)
 public abstract class MixinTrunkPlacer {
-    @Inject(method = "isDirt", at = @At("HEAD"), cancellable = true)
-    private static void isDirt(LevelSimulatedReader level, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(method = "setDirtAt", at = @At("HEAD"), cancellable = true)
+    private static void cancelPlaceDirt(LevelSimulatedReader level, BiConsumer<BlockPos, BlockState> setter, Random random, BlockPos pos, TreeConfiguration config, CallbackInfo ci) {
         if (level.isStateAtPosition(pos, state ->
             state.getBlock() == AutoPlanter.Holder.PLANTER_BLOCK ||
                 state.getBlock() == AutoPlanter.Holder.PLANTER_UPGRADED_BLOCK)) {
-            cir.setReturnValue(Boolean.TRUE);
+            ci.cancel();
         }
     }
 }
