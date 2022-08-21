cdc268c7b30987d5bb90d75d634827b07d47d3e0
Kotori316 <Kotori316@gmail.com>
2022-08-21 15:51:11 +0900

    Update to follow changes in capability.

--- src/main/java/com/kotori316/auto_planter/planter/PlanterTile.java
+++ src/main/java/com/kotori316/auto_planter/planter/PlanterTile.java
@@ -22,8 +22,8 @@ import net.minecraft.world.level.block.CropBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandlerModifiable;
 import net.minecraftforge.items.wrapper.InvWrapper;
 import org.jetbrains.annotations.NotNull;
@@ -79,7 +79,7 @@ public abstract class PlanterTile extends BlockEntity implements Container, Menu
     @Nonnull
     @Override
     public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
-        if (cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY)
+        if (cap == ForgeCapabilities.ITEM_HANDLER)
             return handlerLazyOptional.cast();
         return super.getCapability(cap, side);
     }
