10554dfd74c04a64fb612b99a121c7150d5558b6
Kotori316 <Kotori316@gmail.com>
2026-09-21 15:09:42 +0900

    Remove mixin for forge

--- forge/src/main/java/com/kotori316/auto_planter/forge/planter/PlanterBlockForge.java
+++ forge/src/main/java/com/kotori316/auto_planter/forge/planter/PlanterBlockForge.java
@@ -13,10 +13,11 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SaplingBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.common.IPlantable;
-import net.minecraftforge.common.PlantType;
 import net.minecraftforge.common.ToolAction;
 import net.minecraftforge.common.ToolActions;
 import net.minecraftforge.common.util.Result;
@@ -57,12 +58,14 @@ public sealed abstract class PlanterBlockForge extends PlanterBlock {
 
     @Override
     public final boolean canSustainPlant(BlockState state, BlockGetter world, BlockPos pos, Direction facing, IPlantable plantable) {
-        PlantType type = plantable.getPlantType(world, pos.relative(facing));
-        if (state.getValue(TRIGGERED)) {
-            return type == PlantType.PLAINS || type == PlantType.CROP;
-        } else {
-            return type == PlantType.PLAINS;
+        Block block = plantable.getPlant(world, pos.relative(facing)).getBlock();
+        if (block instanceof SaplingBlock) {
+            return true;
         }
+        if (state.getValue(TRIGGERED) && PlanterTile.isPlantableCrop(block)) {
+            return true;
+        }
+        return super.canSustainPlant(state, world, pos, facing, plantable);
     }
 
     @Override
